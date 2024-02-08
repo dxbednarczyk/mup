85567996d99988e365d36dd7f3f5f0cb93ef199e
Damian Bednarczyk <damian@bednarczyk.xyz>
2024-02-08 10:56:32 -0600

    add check for properly initialized lockfile

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,7 +1,10 @@
+use anyhow::anyhow;
 use clap::Subcommand;
 
 mod eula;
 pub mod lockfile;
+
+use lockfile::Lockfile;
 
 #[derive(Debug, Subcommand)]
 pub enum Server {
@@ -25,10 +28,18 @@ pub fn action(server: &Server) -> Result<(), anyhow::Error> {
         Server::Init {
             minecraft_version,
             loader,
-        } => {
-            lockfile::Lockfile::with_params(minecraft_version, loader)?;
-            Ok(())
-        }
+        } => init(minecraft_version, loader),
         Server::Sign => eula::sign(),
     }
+}
+
+fn init(minecraft_version: &str, loader: &str) -> Result<(), anyhow::Error> {
+    let mut lf = Lockfile::with_params(minecraft_version, loader)?;
+    if !lf.is_initialized() {
+        return Err(anyhow!(
+            "lockfile was initialized with invalid configuration"
+        ));
+    }
+
+    Ok(())
 }
